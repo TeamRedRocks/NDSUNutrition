0a04581fd0b2b430f0393937962ed34e5ce042aa
Brett Chastain <Freet94@hotmail.com>
2016-12-01 19:43:09 -0600

    Completed MenuItem Tests

--- NDSUNutrition/App_Engine/src/test/java/AppEngineTests/MenuItemTest.java
+++ NDSUNutrition/App_Engine/src/test/java/AppEngineTests/MenuItemTest.java
@@ -1,8 +1,15 @@
 package AppEngineTests;
 
+import com.Engine.MenuItem;
+import com.Engine.Nutrient;
+import com.Engine.NutrientTotal;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -24,78 +31,158 @@ public class MenuItemTest {
 
     @Test
     public void equals() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+        MenuItem m2 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        boolean testBool =  m1.equals(m2);
+        assertEquals(testBool, true);
 
     }
 
 
     @Test
     public void getName() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        assertEquals(m1.getName(), "Spaghetti");
 
     }
 
     @Test
     public void setName() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        m1.setName("Pasta");
+
+        assertEquals(m1.getName(), "Pasta");
     }
 
     @Test
     public void getServingSize() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        assertEquals(m1.getServingSize(), 50.0);
     }
 
     @Test
     public void setServingSize() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        m1.setServingSize(200.0);
+
+        assertEquals(m1.getServingSize(), 200.0);
+
 
     }
 
     @Test
     public void getServingUnit() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        assertEquals(m1.getServingUnit(), "ounces");
     }
 
     @Test
     public void setServingUnit() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        m1.setServingUnit("pounds");
+
+        assertEquals(m1.getServingSize(), "pounds");
     }
 
     @Test
     public void getNutrients() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        List<NutrientTotal> nutrientList2 = new ArrayList<>();
+
+        assertEquals(m1.getNutrients(), nutrientList2);
 
     }
 
     @Test
     public void setNutrients() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        List<NutrientTotal> nutrientListToBeSet = new ArrayList<>();
+
+        Nutrient newNutrient = new Nutrient("B12", "Miligrams" );
+
+        NutrientTotal newTotal = new NutrientTotal(newNutrient, 1);
+
+        nutrientListToBeSet.add(newTotal);
+
+        m1.setNutrients(nutrientListToBeSet);
+
+        assertEquals(m1.getNutrients(), nutrientListToBeSet);
     }
 
     @Test
     public void getCategory() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        assertEquals(m1.getCategory(), "Entree");
 
     }
 
     @Test
     public void setCategory() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        m1.setCategory("Dessert");
+
+        assertEquals(m1.getCategory(), "Dessert");
     }
 
     @Test
     public void getInfoSource() throws Exception {
 
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        assertEquals(m1.getInfoSource(), "Google.com");
+
     }
 
     @Test
     public void setInfoSource() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
+
+        m1.setInfoSource("Bing.com");
+
+        assertEquals(m1.getInfoSource(), "Bing.com");
 
     }
 
     @Test
     public void getVenue() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        assertEquals(m1.getVenue(), "Johnny Carinos");
     }
 
     @Test
     public void setVenue() throws Exception {
+        List<NutrientTotal> nutrientList = new ArrayList<>();
+        MenuItem m1 = new MenuItem("Spaghetti", 50.0, "ounces", nutrientList, "Entree", "Google.com", "Johnny Carinos");
 
+        m1.setVenue("Olive Garden");
+
+        assertEquals(m1.getVenue(), "Olive Garden");
     }
 
 }
